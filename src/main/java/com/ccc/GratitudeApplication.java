2d9e6383161f100fcc737a52f6e12ca47cb9fcf8
chechiachang <chechiachang999@gmail.com>
2017-02-18 22:07:34 +0800

    Init commit

---
+++ src/main/java/com/ccc/GratitudeApplication.java
@@ -0,0 +1,12 @@
+package com.ccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratitudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratitudeApplication.class, args);
+	}
+}
